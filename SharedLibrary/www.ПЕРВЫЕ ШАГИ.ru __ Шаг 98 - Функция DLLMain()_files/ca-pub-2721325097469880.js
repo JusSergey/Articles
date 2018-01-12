0ac35920ba88650b04b958e535a45449b262e468
JusSergey <jus.noname@gmail.com>
2018-01-12 11:01:07 +0200

    added articler for catch load/unload events of the Shared Library Windows/Linux

---
+++ SharedLibrary/www.ПЕРВЫЕ ШАГИ.ru __ Шаг 98 - Функция DLLMain()_files/ca-pub-2721325097469880.js
@@ -0,0 +1,1 @@
+try{window.localStorage.setItem('google_pub_config','{"sraConfigs":{"4":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"18w7l6h,3":1,"zowmeu,2":1}},"2":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"zowmeu,2":1,"18w7l6h,3":1}}}}');}catch(e){}
